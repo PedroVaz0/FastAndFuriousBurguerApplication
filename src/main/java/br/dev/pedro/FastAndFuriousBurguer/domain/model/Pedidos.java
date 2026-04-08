08497d90b82d9dcccf695a427be54dd65c0b5e4b
Pedro <ph7519490@gmail.com>
2026-04-08 17:12:27 -0300

    implementação dos atributos em seus metodos

---
+++ src/main/java/br/dev/pedro/FastAndFuriousBurguer/domain/model/Pedidos.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.dev.pedro.FastAndFuriousBurguer.domain.model;
+
+/**
+ *
+ * @author sesi3dib
+ */
+public class Pedidos {
+    private long id;
+    private String nome;
+    private String telefone;
+    private String email;
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 23 * hash + (int) (this.id ^ (this.id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pedidos other = (Pedidos) obj;
+        return this.id == other.id;
+    }
+
+    public Pedidos(long id, String nome, String telefone, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Pedidos() {
+    }
+}
