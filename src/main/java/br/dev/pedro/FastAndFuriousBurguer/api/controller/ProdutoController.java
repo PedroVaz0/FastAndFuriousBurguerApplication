1e8de137a095fabf5530e76079f305eb62e25189
Pedro <ph7519490@gmail.com>
2026-04-15 17:10:18 -0300

    implementação do metodo get e post do produto inicio do metodo put

--- src/main/java/br/dev/pedro/FastAndFuriousBurguer/api/controller/ProdutoController.java
+++ src/main/java/br/dev/pedro/FastAndFuriousBurguer/api/controller/ProdutoController.java
@@ -4,10 +4,72 @@
  */
 package br.dev.pedro.FastAndFuriousBurguer.api.controller;
 
+import br.dev.pedro.FastAndFuriousBurguer.domain.model.Pedido;
+import br.dev.pedro.FastAndFuriousBurguer.domain.model.Produto;
+import br.dev.pedro.FastAndFuriousBurguer.domain.repository.ProdutoRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import static org.springframework.http.ResponseEntity.notFound;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author sesi3dib
  */
+
+@RestController
 public class ProdutoController {
     
+    @Autowired
+    private ProdutoRepository produtoRepository;
+    
+    @GetMapping("/produto")
+    public List<Produto> listas() {
+        return produtoRepository.findAll();
+    }
+    
+    
+    @GetMapping("/produto/{produtoID}")
+    public ResponseEntity<Produto> buscar(@PathVariable Long produtoID) {
+        Optional<Produto> produto = produtoRepository.findById(produtoID);
+        
+        if(produto.isPresent()) {
+            return ResponseEntity.ok(produto.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    
+    
+    @PostMapping("/produto")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto adicionar(@RequestBody Produto produto) {
+        return produtoRepository.save(produto);
+    }
+    
+    
+    @PutMapping("/produto/{clienteID}")
+    public ResponseEntity<Produto> atualizar(@PathVariable Long produtoID,
+            @RequestBody Produto produto) {
+        
+        
+        if (!produtoRepository.existsById(produtoID)) {
+            return ResponseEntity.notFound().build();
+        }
+        
+        produto.setId(produtoID);
+        produto = produtoRepository.save(produto);
+        return ResponseEntity.ok(produto);
+    }
+     
 }
--- src/main/java/br/dev/pedro/FastAndFuriousBurguer/domain/model/Produto.java
+++ src/main/java/br/dev/pedro/FastAndFuriousBurguer/domain/model/Produto.java
@@ -5,6 +5,8 @@
 package br.dev.pedro.FastAndFuriousBurguer.domain.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 /**
@@ -14,8 +16,75 @@ import jakarta.persistence.Id;
 @Entity
 public class Produto {
     @Id
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
-    private double preco;
+    private Double preco;
     private String ingredientes;
+    
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + (int) (this.id ^ (this.id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Produto other = (Produto) obj;
+        return this.id == other.id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public Produto(Long id, String nome, Double preco, String ingredientes) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.ingredientes = ingredientes;
+    }
+
+    public Produto() {
+    }
 }
